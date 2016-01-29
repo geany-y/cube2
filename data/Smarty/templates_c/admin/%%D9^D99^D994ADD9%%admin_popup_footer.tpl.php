144f8802a4b5b7a8c80be813c1e174733c70c81e
U-LOCKON\yasumasa_yoshinaga <yasumasa_yoshinaga@PC-0107.lockon.local>
2016-01-29 13:39:04 +0900

    first commit

---
+++ data/Smarty/templates_c/admin/%%D9^D99^D994ADD9%%admin_popup_footer.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.27, created on 2016-01-28 17:57:34
+         compiled from C:/xampp/htdocs/cube2/html/../data/Smarty/templates/admin/admin_popup_footer.tpl */ ?>
+
+</div>
+</body>
+</html>
---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost/cube2/html/');
+define('HTTPS_URL', 'http://localhost/cube2/html/');
+define('ROOT_URLPATH', '/cube2/html/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'cube2_0128');
+define('DB_PORT', '3306');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'heslioclameaprioloustaebaisluphuthihoucr');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
